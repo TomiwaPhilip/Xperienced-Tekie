0db1d8c494b1d5e970cbaaf5d55a33709ac71410
Tomiwa Philip <110630370+TomiwaPhilip@users.noreply.github.com>
2023-10-23 16:01:53 +0000

    Building

--- app/(root)/(admin)/admin/page.jsx
+++ app/(root)/(admin)/admin/page.jsx
@@ -1,10 +1,25 @@
+"use client"
+
+import { redirect } from 'next/navigation';
+import { useUser } from '@clerk/nextjs';
+
 import DashboardNav from '../../../../components/dashboard/DashboardNav';
+import BarsContent from '../../../../components/dashboard/BarsContent';
 
 const Page = () => {
+  const { user } = useUser();
+  const clerkId = "user_2WZNhGBCcvBfCn9Dri7CUsCZGTJ"; // Replace with the clerk's user ID
+
+  if (user && user.id !== clerkId) {
+    // Redirect to the dashboard page if the user IDs don't match
+    alert("Restricted page for admins only!!!");
+    redirect('/dashboard');
+  };
 
   return (
     <section>
       <DashboardNav />
+      <BarsContent />
     </section>
   );
 };
--- app/(root)/(admin)/layout.jsx
+++ app/(root)/(admin)/layout.jsx
@@ -1,7 +1,4 @@
 import { ClerkProvider } from "@clerk/nextjs";
-import { redirect } from 'next/navigation';
-import { useUser } from '@clerk/nextjs';
-import { withServerSideAuth } from '@clerk/nextjs/api';
 
 import "../../globals.css";
 
@@ -10,18 +7,7 @@ export const metadata = {
   description: "Xperienced Tekie",
 };
 
-const getServerSideProps = withServerSideAuth({ loadUser: true});
-
 const RootLayout = ({ children }) => {
-  const { user } = useUser();
-  const clerkId = "user_2WZNhGBCcvBfCn9Dri7CUsCZGTJ"; // Replace with the clerk's username
-
-  if (user && user.id !== clerkId) {
-    // Redirect to the dashboard page if the user IDs don't match
-    alert("Restricted page for admins only!!!");
-    redirect('/dashboard');
-  };
-
   return (
     <ClerkProvider>
       <html lang='en'>
@@ -33,4 +19,4 @@ const RootLayout = ({ children }) => {
   );
 }
 
-export default { getServerSideProps, RootLayout };
+export default RootLayout;
