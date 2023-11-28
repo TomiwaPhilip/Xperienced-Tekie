24b31f843c1091f316eeb175263a436abb5d6f7f
JosephIgbaji <igbajijoseph.k@gmail.com>
2023-11-28 02:32:09 +0100

    styled terms page

--- app/(root)/(terms)/terms-condition/page.jsx
+++ app/(root)/(terms)/terms-condition/page.jsx
@@ -1,28 +1,28 @@
 const Terms = () => {
   return (
-    <div>
-      <p>
+    <div className="container mx-auto px-16">
+      <p className="mb-5 text-xl">
         Body Bold Extra Large. Most fonts have a particular weight which
         corresponds to one of the numbers in Common weight name mapping. However
         some fonts, called variable fonts, can support a range of weights with a
         more or less fine granularity, and this can give the designer a much
         closer degree of control..
       </p>
-      <p>
+      <p className="mb-5 text-lg">
         Body Bold Large. Most fonts have a particular weight which corresponds
         to one of the numbers in Common weight name mapping. However some fonts,
         called variable fonts, can support a range of weights with a more or
         less fine granularity, and this can give the designer a much closer
         degree of control..
       </p>
-      <p>
+      <p className="mb-5 text-base">
         Body Bold Small. Most fonts have a particular weight which corresponds
         to one of the numbers in Common weight name mapping. However some fonts,
         called variable fonts, can support a range of weights with a more or
         less fine granularity, and this can give the designer a much closer
         degree of control..
       </p>
-      <p>
+      <p className="mb-5 text-sm">
         Body Bold Extra Small. Most fonts have a particular weight which
         corresponds to one of the numbers in Common weight name mapping. However
         some fonts, called variable fonts, can support a range of weights with a
