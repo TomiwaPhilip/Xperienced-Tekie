f0f59d6e3391d7f4e5d06a82aa55ad1fda7a1ebe
Tomiwa Philip <110630370+TomiwaPhilip@users.noreply.github.com>
2023-12-29 02:05:28 +0000

    fixed certificate bugs

--- components/dashboard/devCard/CertDownloadForm.jsx
+++ components/dashboard/devCard/CertDownloadForm.jsx
@@ -35,30 +35,29 @@ const CertDownloadForm = () => {
 
     // Load the image
     var img = new Image();
-    img.src = "/assets/images/Xperienced Tekie (1).png";
+    img.src = "/assets/images/Xperienced Tekie (3).png";
 
     // Add the image to the document
     doc.addImage(img, 0, 0, 297, 210);
 
     // Get the dimensions of the bigger rectangle
     var bigRect = {
-      x: 106,
-      y: 116,
+      x: 10,
+      y: 118,
       width: 170,
       height: 100,
     };
 
     // Get the dimensions of the smaller rectangle
     var smallRect = {
-      x: 127,
-      y: 127,
+      x: 76,
+      y: 132,
       width: 170,
       height: 45,
     };
 
     // Add text to the bigger rectangle
-    doc.setFontSize(40);
-    doc.setFont("Courier-Bold");
+    doc.setFontSize(25);
     doc.setTextColor(0, 0, 0); // Black
     doc.text(
       bigRect.x + 5,
@@ -84,6 +83,7 @@ const CertDownloadForm = () => {
         Weldone on achieving this feat!! Please click the below button to
         download your certificate.
       </p>
+      <br /> <br />
       <button
         type="submit"
         className="flex text-white bg-blue-950 px-5 items-center rounded-lg"
