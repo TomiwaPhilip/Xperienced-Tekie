18555421d82470781c7b3bc03675887b4833b2b9
Tomiwa Philip <110630370+TomiwaPhilip@users.noreply.github.com>
2023-12-30 12:39:35 +0000

    Create page.jsx and PrivacyPolicy.jsx

---
+++ app/(root)/(path-pages)/privacy-policy/page.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import PrivacyPolicy from "../../../../components/shared/PrivacyPolicy";
+
+const Home = () => {
+  return (
+    <div className="">
+        <PrivacyPolicy />
+    
+    
+    </div>
+  );
+};
+
+export default Home;
---
+++ components/shared/PrivacyPolicy.jsx
@@ -0,0 +1,112 @@
+import React from "react";
+
+const PrivacyPolicy = () => {
+  return (
+    <div>
+      <h1>Privacy Policy for Xperienced Tekie</h1>
+      <p>Last Updated: [Date]</p>
+
+      <h2>Welcome to Xperienced Tekie!</h2>
+      <p>
+        This Privacy Policy is designed to help you understand how we collect,
+        use, disclose, and safeguard your personal information. By using our
+        website, products, and services, you consent to the practices outlined
+        in this Privacy Policy.
+      </p>
+
+      <h2>Information We Collect</h2>
+      <p>
+        <strong>Personal Information:</strong>
+      </p>
+      <p>
+        We may collect personal information such as your name, email address,
+        and contact details when you use our services or interact with our
+        website.
+      </p>
+
+      <p>
+        <strong>Usage Information:</strong>
+      </p>
+      <p>
+        We collect information about your interactions with our website and
+        services, including IP addresses, browser type, pages visited, and other
+        usage data.
+      </p>
+
+      <h2>How We Use Your Information</h2>
+      <p>We use the collected information for the following purposes:</p>
+
+      <p>
+        <strong>Providing Services:</strong>
+      </p>
+      <p>
+        To deliver the services you have requested, process transactions, and
+        ensure the functionality of our products.
+      </p>
+
+      <p>
+        <strong>Communication:</strong>
+      </p>
+      <p>
+        To communicate with you, respond to your inquiries, and provide customer
+        support.
+      </p>
+
+      <p>
+        <strong>Improvement of Services:</strong>
+      </p>
+      <p>
+        To analyze and improve our products and services, and to develop new
+        features and offerings.
+      </p>
+
+      <p>
+        <strong>Marketing and Promotions:</strong>
+      </p>
+      <p>
+        With your consent, we may use your information to send you promotional
+        materials, newsletters, or other marketing communications.
+      </p>
+
+      <h2>Information Sharing</h2>
+      <p>
+        We do not sell, trade, or otherwise transfer your personal information
+        to outside parties. However, we may share information with trusted third
+        parties who assist us in operating our website, conducting our business,
+        or servicing you.
+      </p>
+
+      <h2>Security</h2>
+      <p>
+        We take reasonable measures to protect your personal information from
+        unauthorized access, disclosure, alteration, and destruction. However,
+        no method of transmission over the internet or electronic storage is
+        100% secure.
+      </p>
+
+      <h2>Your Choices</h2>
+      <p>You have the right to:</p>
+
+      <ul>
+        <li>Access, correct, or delete your personal information.</li>
+        <li>Opt-out of receiving marketing communications.</li>
+      </ul>
+
+      <h2>Changes to this Privacy Policy</h2>
+      <p>
+        We may update this Privacy Policy from time to time. The date of the
+        latest revision will be indicated at the top of the page.
+      </p>
+
+      <h2>Contact Us</h2>
+      <p>
+        If you have any questions about this Privacy Policy, please contact us
+        at <a href="mailto:contact@email.com">contact@email.com</a>.
+      </p>
+
+      <p>Thank you for choosing Xperienced Tekie!</p>
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
